4fe19d63d8bbdb3fcead978e19bd6710d320d776
shreeyashl333 <lahane.s@northeastern.edu>
2025-10-04 12:24:32 -0400

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,45 @@
 import sys
-from src.logger import logging
+import logging
+from types import TracebackType
+from typing import Optional, Tuple, Type
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+from src.logger import logging   # your custom logger
 
-    return error_message
 
-    
+def error_message_detail(
+    error: Exception,
+    error_detail: Tuple[Optional[Type[BaseException]], Optional[BaseException], Optional[TracebackType]]
+) -> str:
+    """
+    Returns a formatted error message with filename, line number, and error details.
+    """
+    _, _, exc_tb = error_detail
+    file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown"
+    line_no = exc_tb.tb_lineno if exc_tb else "Unknown"
+
+    return (
+        f"Error occurred in script [{file_name}] "
+        f"at line [{line_no}] "
+        f"with error: [{str(error)}]"
+    )
+
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
+    """
+    Custom exception class that captures detailed traceback info
+    and logs the error automatically.
+    """
+
+    def __init__(
+        self,
+        error: Exception,
+        error_detail: Tuple[Optional[Type[BaseException]], Optional[BaseException], Optional[TracebackType]]
+    ):
+        super().__init__(str(error))  # base exception keeps error string
+        self.error_message = error_message_detail(error, error_detail)
+
+        # log the error automatically
+        logging.error(self.error_message)
+
     def __str__(self):
-        return self.error_message 
+        return self.error_message
